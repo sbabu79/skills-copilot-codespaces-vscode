8aa5da45743415586186ba4f5a5167384fe93e5c
sbabu79 <srbabu9000@gmail.com>
2024-06-17 05:55:11 +0000

    copilot 2.2 commit

--- member.js
+++ member.js
@@ -1,12 +1,9 @@
-function skillsmember() {  
-  var member = document.getElementById("member").value;
-  var skills = document.getElementById("skills").value;
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("skillsmember").innerHTML = this.responseText;
-    }
+function skillsmember() {
+  return {
+    restrict: 'E',
+    scope: {
+      member: '='
+    },
+    templateUrl: 'member.html'
   };
-  xhttp.open("GET", "skillsmember.php?member="+member+"&skills="+skills, true);
-  xhttp.send();
 }
